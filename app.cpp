21dde187914beda750be5fe7b714766c3512ee29
zulfikra l.abdul <zulfikralahmudin@gmail.com>
2020-05-24 03:11:27 +0700

    add new code

--- app.cpp
+++ app.cpp
@@ -1,24 +1,76 @@
 #include <wx/wx.h>
 
-class Simple : public wxFrame
-{
-    public:
-        Simple(const wxString& title) 
-            : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
-            {
-                Centre();
-            }
-};
 
 class MyApp : public wxApp
 {
     public:
-        bool OnInit()
-        {
-            Simple *simple = new Simple(wxT("Simple"));
-            simple->Show(true);
-            return true;
-        }
+        virtual bool OnInit();
 };
 
+class MyFrame : public wxFrame
+{
+    public:
+        MyFrame(const wxString& title, const wxPoint& post, const wxSize& size);
+    private:
+        void OnHello(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+
+        wxDECLARE_EVENT_TABLE();
+};
+
+enum {
+    ID_Hello = 1
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Hello, MyFrame::OnHello)
+    EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+wxEND_EVENT_TABLE()
+
 wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello world",wxPoint(50,50), wxSize(450,340));
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size): wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello....\tCtrl-H",
+                    "Help string show in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&help");
+
+    SetMenuBar( menuBar );
+
+    CreateStatusBar();
+    SetStatusText("welcome to my apps");
+}
+
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("this is on about call",
+                "About hello world", wxOK | wxICON_INFORMATION);
+}
+
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxwidget");
+}
